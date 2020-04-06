351df3f20136d15ef53172d2deda0696436b5007
angelfraga <angel.fraga.parodi@leanix.net>
2020-04-06 22:54:09 +0200

    add initial route component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,25 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { GsecAppModule } from './gsec-app/gsec-app.module';
-import { Route } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
+    RouterModule,
     GsecAppModule.forRoot([
+      { 
+        path: '', component: HomeComponent 
+      } as Route,
       { 
         path: 'settings',
         loadChildren:() => import('./settings/settings.module').then(mod => mod.SettingsModule)
       } as Route
     ])
   ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HomeComponent  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
